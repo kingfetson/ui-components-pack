b7f0835a90e91fec65d7c8aa173661106f2e9369
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2026-03-13 20:55:07 +0300

    Add mobile menu toggle functionality

---
+++ navbar-03-sticky/script.js
@@ -0,0 +1,7 @@
+// Toggle menu for mobile
+const hamburger = document.getElementById('hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('show');
+});
