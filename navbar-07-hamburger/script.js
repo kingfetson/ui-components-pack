6422f134b73d57e241979bf8c087ea7c28ce77bf
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2026-03-14 10:48:36 +0300

    Add hamburger menu toggle functionality

---
+++ navbar-07-hamburger/script.js
@@ -0,0 +1,7 @@
+// Hamburger toggle
+const hamburger = document.getElementById('hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('show');
+});
